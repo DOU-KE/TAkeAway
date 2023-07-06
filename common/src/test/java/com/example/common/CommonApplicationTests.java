7e3cee96d0fbaf0d8b2d2a865a0368637d4e02e4
douke <3049832065@qq.com>
2023-07-06 15:16:15 +0800

    原始数据

---
+++ common/src/test/java/com/example/common/CommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.common;
+
+import org.testng.annotations.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ pojo/src/test/java/com/example/pojo/PojoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pojo;
+
+import org.testng.annotations.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PojoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
